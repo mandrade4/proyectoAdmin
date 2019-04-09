76ee73524234100f570966e417e06d177487c694
Marco Andrade <mandrade0412@gmail.com>
2019-04-09 16:15:33 -0500

    Agregando engine de Text-PDF

---
+++ Audata/src/main/java/pe/edu/ulasalle/dima/audata/text_pdf/engine/IReaderPdf.java
@@ -0,0 +1,5 @@
+package pe.edu.ulasalle.dima.audata.text_pdf.engine;
+
+public interface IReaderPdf {
+
+}
---
+++ Audata/src/main/java/pe/edu/ulasalle/dima/audata/text_pdf/engine/ReaderPdfImpl.java
@@ -0,0 +1,5 @@
+package pe.edu.ulasalle.dima.audata.text_pdf.engine;
+
+public class ReaderPdfImpl implements IReaderPdf {
+
+}
