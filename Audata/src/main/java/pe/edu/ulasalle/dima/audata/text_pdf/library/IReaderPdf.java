b9310c08d59cb8dd8a90ef94b971901085184dee
Marco Andrade <mandrade0412@gmail.com>
2019-04-11 17:56:26 -0500

    Subiendo dependencia de PDFBox en pom.xml. Implementacion de la funcion Leer PDF.

--- Audata/src/main/java/pe/edu/ulasalle/dima/audata/text_pdf/library/IReaderPdf.java
+++ Audata/src/main/java/pe/edu/ulasalle/dima/audata/text_pdf/library/IReaderPdf.java
@@ -1,5 +1,17 @@
 package pe.edu.ulasalle.dima.audata.text_pdf.library;
 
+//import org.apache.pdfbox.pdmodel.PDDocument;
+//import org.apache.pdfbox.text.PDFTextStripper;
+//import org.apache.pdfbox.text.PDFTextStripperByArea;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
 public interface IReaderPdf {
 
+	public String readPDF( FileInputStream fstream ) throws FileNotFoundException, IOException;
+	
 }
--- Audata/src/main/java/pe/edu/ulasalle/dima/audata/text_pdf/library/ReaderPdfImpl.java
+++ Audata/src/main/java/pe/edu/ulasalle/dima/audata/text_pdf/library/ReaderPdfImpl.java
@@ -1,5 +1,48 @@
 package pe.edu.ulasalle.dima.audata.text_pdf.library;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+//import java.io.File;
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.IOException;
+//import java.io.InputStream;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.pdfbox.text.PDFTextStripperByArea;
+
 public class ReaderPdfImpl implements IReaderPdf {
 
+	public String readPDF(FileInputStream fstream) throws IOException {
+		
+		InputStream instream = fstream;
+		PDDocument document = PDDocument.load(instream);
+    	StringBuilder str = new StringBuilder();
+		
+		document.getClass();
+
+        if (!document.isEncrypted()) {
+		
+            PDFTextStripperByArea stripper = new PDFTextStripperByArea();
+            stripper.setSortByPosition(true);
+
+            PDFTextStripper tStripper = new PDFTextStripper();
+
+            String pdfFileInText = tStripper.getText(document);
+
+            String lines[] = pdfFileInText.split("\\r?\\n");
+            for (String line : lines) {
+            	str.append(line);
+            }
+        }
+        
+		return str.toString();
+		
+	}
+	
+
+	
 }
