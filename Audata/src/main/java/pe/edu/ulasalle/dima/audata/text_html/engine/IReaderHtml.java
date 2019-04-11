a99a23055281b18839cf914f6af64f0b5bf90a74
Edu <phuallancapo@ulasalle.edu.pe>
2019-04-11 14:25:45 -0500

    Commit

---
+++ Audata/src/main/java/pe/edu/ulasalle/dima/audata/text_html/engine/IReaderHtml.java
@@ -0,0 +1,5 @@
+package pe.edu.ulasalle.dima.audata.text_html.engine;
+
+public interface IReaderHtml {
+
+}
---
+++ Audata/src/main/java/pe/edu/ulasalle/dima/audata/text_html/library/ReaderHtmlImpl.java
@@ -0,0 +1,5 @@
+package pe.edu.ulasalle.dima.audata.text_html.library;
+
+public class ReaderHtmlImpl {
+
+}
