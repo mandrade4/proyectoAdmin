acfec892aa03eab81b4b21471b5d42cfdc5df839
JefryDomingo <jacunam@ulasalle.edu.pe>
2019-04-11 18:11:23 -0500

    Agregando metodo de conversion de texto a bytes

--- Audata/src/main/java/pe/edu/ulasalle/dima/audata/tts/library/TtsSpanishDefault.java
+++ Audata/src/main/java/pe/edu/ulasalle/dima/audata/tts/library/TtsSpanishDefault.java
@@ -2,13 +2,18 @@ package pe.edu.ulasalle.dima.audata.tts.library;
 
 import pe.edu.ulasalle.dima.audata.tts.controller.ITts;
 
-public class TtsSpanishDefault implements ITts {
+public class TtsSpanishDefault extends TtsGenerico implements ITts {
 
 	public TtsSpanishDefault() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
 	
+	public byte[] mp3(String text) {
+		byte[] b = text.getBytes();
+		return b;
+		
+	}
 	
 	
 
