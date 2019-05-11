a64d7d842df59e61a72b377ae69971c10998a245
JefryDomingo <jacunam@ulasalle.edu.pe>
2019-05-11 13:05:05 -0500

    Arreglando errores de compatibilidad
    Nota: Asegurense cada vez que hagan un push de verificar la version de java que estan utilizando que es un dolor de cabeza y perdida de tiempo arreglar este tipo de cosas. Lo que hice es agregar una clase configuracion que sera la URL raiz-
    Posdata: No cambia nada la forma como han estado compilando, sigan haciendolo de la misma manera.

---
+++ Audata/src/main/java/pe/edu/ulasalle/dima/audata/test/Configuration.java
@@ -0,0 +1,9 @@
+package pe.edu.ulasalle.dima.audata.test;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/service")
+public class Configuration extends Application {
+
+}
