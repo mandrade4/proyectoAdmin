5ff03a0d8d4c4e588aaa239ff8feaccb28265bd5
JefryDomingo <jacunam@ulasalle.edu.pe>
2019-05-27 19:50:46 -0500

    Organizando arquitectura etapa 2

--- Audata/src/main/java/pe/edu/ulasalle/dima/audata/tts/library/TtsGenerico.java
+++ Audata/src/main/java/pe/edu/ulasalle/dima/audata/tts/library/TtsGenerico.java
@@ -2,5 +2,18 @@ package pe.edu.ulasalle.dima.audata.tts.library;
 
 public class TtsGenerico {
 	
+	protected String language;
+
+	public String getLanguage() {
+		
+		return language;
+		
+	}
+
+	public void setLanguage(String language) {
+		
+		this.language = language;
+		
+	}
 	
 }
